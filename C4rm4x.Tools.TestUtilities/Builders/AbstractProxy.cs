e51ead0d01ae6643377042c2ca1ad6690e098459
Carmelo Quintana <carmeloquintanahernandez@hotmail.com>
2017-05-27 20:51:12 +0100

    Adding test builders

---
+++ C4rm4x.Tools.TestUtilities/Builders/AbstractProxy.cs
@@ -0,0 +1,53 @@
+﻿#region Using
+
+using Moq;
+using System;
+using System.Linq.Expressions;
+
+#endregion
+
+namespace C4rm4x.Tools.TestUtilities.Builders
+{
+    /// <summary>
+    /// Base class to build random proxies of type T
+    /// </summary>
+    /// <typeparam name="T">The type of the proxy to create</typeparam>
+    public abstract class AbstractProxy<T>
+        where T : class
+    {
+        /// <summary>
+        /// The proxy created
+        /// </summary>
+        protected readonly T _entity;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AbstractProxy()
+        {
+            _entity = Mock.Of<T>();
+        }
+
+        /// <summary>
+        /// Returns the proxy created
+        /// </summary>
+        /// <returns></returns>
+        public T Build()
+        {
+            return _entity;
+        }
+
+        /// <summary>
+        /// Sets property getter
+        /// </summary>
+        /// <typeparam name="TProperty">Type of the property</typeparam>
+        /// <param name="expression">Lambda expression that specifies the property getter</param>
+        /// <param name="value">The value</param>
+        protected void SetProperty<TProperty>(
+            Expression<Func<T, TProperty>> expression,
+            TProperty value)
+        {
+            Mock.Get(_entity).SetupGet(expression).Returns(value);
+        }
+    }
+}
---
+++ C4rm4x.Tools.TestUtilities/Builders/BuilderCollection.cs
@@ -0,0 +1,40 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace C4rm4x.Tools.TestUtilities.Builders
+{
+    /// <summary>
+    /// Base class to build a collection of random instances
+    /// </summary>
+    public static class BuilderCollection
+    {
+        /// <summary>
+        /// Generates the random collection
+        /// </summary>
+        /// <typeparam name="TEntity">Type of the entities to build</typeparam>
+        /// <param name="generator">Entities generator</param>
+        /// <param name="minNumberOfItems">Min number of items</param>
+        /// <param name="maxNumberOfItems">Max number of items</param>
+        /// <returns>The collection of random items</returns>
+        public static IEnumerable<TEntity> Generate<TEntity>(
+            Func<TEntity> generator,
+            int minNumberOfItems = 1,
+            int maxNumberOfItems = 10)
+            where TEntity : class
+        {
+            var numberOfItems = GetRand(minNumberOfItems, maxNumberOfItems);
+
+            for (var i = 0; i < numberOfItems; i++)
+                yield return generator();
+        }
+
+        private static int GetRand(int min, int max)
+        {
+            return new Random().Next(min, max);
+        }
+    }
+}
