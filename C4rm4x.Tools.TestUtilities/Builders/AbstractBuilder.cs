628b97bcd98863cb6057bc0715abb9e440b0ca9d
carmelo <carmelo@purusdigital.com>
2017-06-12 10:30:31 +0100

    Adding support to set private property  (not more internals !!!)

--- C4rm4x.Tools.TestUtilities/Builders/AbstractBuilder.cs
+++ C4rm4x.Tools.TestUtilities/Builders/AbstractBuilder.cs
@@ -1,6 +1,8 @@
 ﻿#region Using
 
 using System;
+using System.Linq.Expressions;
+using System.Reflection;
 
 #endregion
 
@@ -43,6 +45,40 @@ namespace C4rm4x.Tools.TestUtilities.Builders
         public T Build()
         {
             return _entity;
+        }
+
+        /// <summary>
+        /// Sets the value of the given private property
+        /// </summary>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="setter">The expression to get the property to be set</param>
+        /// <param name="value">The new value</param>
+        protected void SetPropertyValue<TProperty>(
+            Expression<Func<T, TProperty>> setter, 
+            TProperty value)
+        {
+            var propertyInfo = GetPropertyInfo(setter);
+
+            propertyInfo.SetValue(_entity, value);
+        }
+
+        private PropertyInfo GetPropertyInfo<TProperty>(Expression<Func<T, TProperty>> setter)
+        {
+            var type = typeof(T);
+
+            var member = setter.Body as MemberExpression;
+            if (member == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a method, not a property.",
+                    setter.ToString()));
+
+            var propInfo = member.Member as PropertyInfo;
+            if (propInfo == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a field, not a property.",
+                    setter.ToString()));
+
+            return propInfo;
         }
     }
 }
